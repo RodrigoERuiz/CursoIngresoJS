5e737a8ac94f8adec1aefa80aa50c2ae2d20f233
unknown <ruizunq@gmail.com>
2023-01-28 21:46:51 -0300

    cambios noche del sabado

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,12 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
 
+	let edad=parseInt(document.getElementById("txtIdEdad").value);
+	let estado=document.getElementById("estadoCivil").value;
+
+	if (edad>18 && estado=="Soltero")
+	{
+		alert("Es soltero y no es menor");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,17 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
-
+	notaExamen=Math.floor(Math.random()*10);
+	alert(notaExamen);
+	if(notaExamen<4)
+	{
+		alert("Vamos la próxima se puede");
+	}
+		else if (notaExamen>=4 && notaExamen<9)
+		{
+			alert("Aprobó");
+		}
+			else
+			{
+				alert("Excelente");
+			}
 }//FIN DE LA FUNCIÓN
